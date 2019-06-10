0173a1a1f9d4dd140420d08e10868c9e770e42a4
apcaplan <apcaplan@gmail.com>
2019-06-10 13:04:39 -0400

    Update

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,9 +1,10 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :date, :length_of_practice, :name_of_practice, :notes, :editable
+  attributes :id, :date, :length_of_practice, :name_of_practice, :notes
+  # :editable
   belongs_to :user
   # has_one :user
 
-  def editable
-    scope == object.user
-  end
+  # def editable
+  #   scope == object.user
+  # end
 end
